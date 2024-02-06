49d3248953e83fba7370b21e24dc0b9fea3914cc
abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-02-06 16:51:16 +0100

    crier les table et ahententification

--- database/migrations/2024_02_06_150510_create_permission_role_table.php
+++ database/migrations/2024_02_06_150510_create_permission_role_table.php
@@ -11,18 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->foreignId('role_id')
-            ->default(4)
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->foreignId('permission_id')
             ->constrained()
             ->cascadeOnDelete()
             ->cascadeOnUpdate();
-            $table->rememberToken();
+
+            $table->foreignId('role_id')
+            ->constrained()
+            ->cascadeOnDelete()
+            ->cascadeOnUpdate();
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('permission_role');
     }
 };
